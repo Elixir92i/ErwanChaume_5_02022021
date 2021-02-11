79f7f59b7e6fb4c6109cf556d7c48f5cecf0a983
Elixir92i <erwan.chaume@gmail.com>
2021-02-11 15:31:55 +0100

    product page (button and design)

--- js/product.js
+++ js/product.js
@@ -6,10 +6,14 @@ async function main() {
         const cameraRaw = await fetch(`http://localhost:3000/api/cameras/${myParam}`).then(r => r.json());
         const camera = new Camera(cameraRaw)
         
-        document.getElementById("imgProduct").setAttribute("src", camera.imageUrl)
+        document.getElementById("camera_name").innerText = camera.name;
+        document.getElementById("camera_image").setAttribute("src", camera.imageUrl)
+        document.getElementById("camera_description").innerHTML = '<span>Description du produit: </span><br />' + camera.description;
+        document.getElementById("camera_price").innerHTML = '<span>Prix: </span>' + camera.price / 100 + '€';
+        document.getElementById("camera_id").innerText = camera.id;
         camera.lenses.forEach((produit)=>{
     		let optionProduit = document.createElement("option");
-    		document.getElementById("lenses_choice").appendChild(optionProduit).innerHTML = produit;
+    		document.getElementById("camera_lenses").appendChild(optionProduit).innerHTML = produit;
     	});
     }
     catch (error) {
